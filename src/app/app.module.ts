7be452fa10f598992bdf458a4fc0aff6ee0a1a5f
Mor Soferian <47902941+MorSof@users.noreply.github.com>
2020-08-18 14:29:10 +0300

    "first commit"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatRippleModule} from '@angular/material/core';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        MatRippleModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
